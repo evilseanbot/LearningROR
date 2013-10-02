cf25e84c58424f3b210b1e724ecf56a86d412058
Seanna Musgrave <evilseanbot@gmail.com>
2013-10-01 20:29:51 -0700

    Made a blank stupid rails app

---
+++ first_ap/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstAp::Application.config.secret_token = 'ebd92ddf305a18d3b27cc4ec3d341021170519da00c33c9bb41878e104623e3587edb2f13baf1e6b9de5873bdfb3d3c4ac0c8cb5be7cef1647654723bb1816af'
